26bbadc226225626f225ebce75d633975bc8e351
anassu1400 <anassu1400@gmail.com>
2019-02-21 10:17:11 +0300

    fixed a problem

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 class IsAdder(BasePermission):
-
+	message = "No, Please Stop!"
 	def has_object_permission(self, request, view, obj):
 		if request.user.is_staff or obj.added_by == request.user:
 			return True
--- api/views.py
+++ api/views.py
@@ -1,20 +1,20 @@
 from rest_framework.generics import (ListAPIView, RetrieveAPIView)
 from items.models import Item, FavoriteItem
 from rest_framework.filters import (OrderingFilter, SearchFilter)
-from rest_framework.permissions import (AllowAny, IsAuthenticated, IsAdminUser) 
+from rest_framework.permissions import (AllowAny,) 
 from .serializers import ItemListSerializer, ItemDetailSerializer
-
+from .permissions import IsAdder
 # Create you here.
 class ItemListView(ListAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemListSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [AllowAny,]
     filter_backends = [OrderingFilter, SearchFilter,]
     search_fields = ['name', 'description']
 
 class ItemDetailView(RetrieveAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemDetailSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAdder, ]
     lookup_field = 'id'
     lookup_url_kwarg = 'item_id'
